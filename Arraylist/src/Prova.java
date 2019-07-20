99d0abdf93ee2d559e854104c854abdb83989537
marco <marco@DESKTOP-PTH873V.lan>
2019-07-20 16:03:17 +0200

    Primo commit esercitazione Arraylist

---
+++ Arraylist/src/Prova.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Prova {
+
+	public static void main(String[] args) {
+		ArrayList<String> array = new ArrayList();
+		array.add("Ciao");
+		array.add("coglione");
+		System.out.println(array);
+		
+
+	}
+
+}
